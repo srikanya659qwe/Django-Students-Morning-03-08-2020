b56a18c8cc8bc4138bfcf10e539b3db7f2aff616
N. Ravi Kumar[NRK] <rravikumar34@gmail.com>
2020-08-12 09:54:11 +0530

    Project folder [Bootstrap concept included]

---
+++ SIP[Boot]/boot/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from boot import views
+
+urlpatterns = [
+    path('',views.home,name="hme"),
+    path('abt/',views.abts,name="ab"),
+    path('rg/',views.regist,name="rgi"),
+]
---
+++ SIP[Boot]/boot/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+	return render(request,'userapp/home.html')
+
+def abts(request):
+	return render(request,'userapp/about.html')
+
+def regist(request):
+	return render(request,'userapp/register.html')
